7baf403f8c9ef25be9ee8c171c2d86a234b742b2
elispelis <0e.bright1@gmail.com>
2021-11-16 23:13:28 +0000

    made plots seperate from each other
    take plot data funyction out

--- Task A.py
+++ Task A.py
@@ -106,11 +106,11 @@ for i in np.linspace(0, int(t_max/dt), int(t_max/dt)+1):
         im = plt.imshow(avphi, cmap=cmap, extent=(x_min, x_max, y_min, y_max)) #Plot using imshow to display data as an image
         axes[-1].set_title('t= '+str(i/1000)+'s') #Shows the time where the subplot was plotted
 
+fig.tight_layout()
 plt.subplots_adjust(right=0.8)
 plt.clim(0, 1) #Colourbar limits
 cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
 cbar = fig.colorbar(im, cax=cbar_ax)
-#cbar = plt.colorbar() #Plot colourbar
 cbar.set_label('Concentration (ϕ)') #Gives colourbar a title
 plt.suptitle('Task A: Advection & Diffusion') #Title of plot
 print("--- %s seconds ---" % (time.time() - start_time)) #Shows code running time
